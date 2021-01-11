bed96d7704893bdfe256fc643306c270842ab620
Nick Renaud <nick_renaud@hotmail.com>
2021-01-11 13:55:00 -0800

    cleanup code

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import Book from './Book'
+import AddBooks from './AddBooks'
 
-function BookShelf({books, shelf, changeShelf}) {
+function BookShelf({ books, shelf, changeShelf }) {
     return (
         <div className="bookshelf">
             <h2 className="bookshelf-title">{shelf}</h2>
-            <div className="bookshelf-books">
-                <ol className="books-grid">
-                    {books.map(book =>
-                        <Book key={book.id} book={book} changeShelf={changeShelf} />
-                    )}
-                </ol>
-            </div>
+
+            {books.length ?
+                <div className="bookshelf-books">
+                    <ol className="books-grid">
+                        {books.map(book =>
+                            <Book key={book.id} book={book} changeShelf={changeShelf} />
+                        )}
+                    </ol>
+                </div>
+                : <AddBooks />
+            }
         </div>
     )
 }
