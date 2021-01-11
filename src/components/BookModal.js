f8395b29896cec50bce963bff18717072bc83d97
Nick Renaud <nick_renaud@hotmail.com>
2021-01-11 13:52:07 -0800

    feat: Display book description
    -When clicking on book cover, opens a modal that displays book
    information including a full description.

---
+++ src/components/BookModal.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+function BookModal({ book, closeModal}) {
+    return (
+        <div className="modal">
+            <div className="modal-content">
+                <span className="close-modal" onClick={() => closeModal()}>×</span>
+
+                <div className="modal-header">
+                    <h2 className="modal-book-title">{book.title}</h2>
+                    <h4 className="modal-book-authors">{book.authors ? book.authors.join(' & ') : ''}</h4>
+                </div>
+                <div className="modal-body">
+                    <div className="modal-book-cover">
+                        <div className="book-cover"
+                            style={{
+                                width: 220,
+                                height: 290,
+                                backgroundImage: book.imageLinks ?
+                                    `url(${book.imageLinks.thumbnail})` : '',
+                                backgroundSize: 'cover',
+                                backgroundRepeat: 'no-repeat'
+                            }}
+                        >
+                        </div>
+                    </div>
+
+                    <div className="modal-book-info">
+                        <p className="book-description">{book.description}</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default BookModal
